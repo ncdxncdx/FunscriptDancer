ddfc1f5032832604901de48722cc9f9867829c77
ncdx <pwwreu00d@mozmail.com>
2022-07-09 17:09:12 +0100

    julia version, needs fixing

--- prototype/audioscripter.sh
+++ prototype/audioscripter.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 set -e
 
 export OFFSET_RANGE=100
---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+using audioscripter
+using Test
+
+@testset "audioscripter.jl" begin
+    @test audioscripter.transform_file("path","name","vamp:vamp-aubio:aubiotempo:beats") == "path/name_vamp_vamp-aubio_aubiotempo_beats.csv"
+    @test audioscripter.base_name("foo/bar/baz.mp4") == "baz"
+    @test audioscripter.offsets([10,20,30,20,10]) == [0,63,100,63,0]
+    @test audioscripter.int_at(110, 20, 90, 10, 100) == 15
+    @test audioscripter.int_at(120, 40, 90, 10, 100) == 20
+    @test audioscripter.int_at(-20, 40, 10, 10, 0) == 20
+    @test audioscripter.peak( 110, 50, 90, 40 ) == [("pos" => 100,"at" => 45),("pos" => 90,"at" => 50)]
+end
